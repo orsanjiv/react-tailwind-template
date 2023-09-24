a611e3353db3fd7562e7cf0de582ad4baa7b8044
SANJIV GUPTA <90419613+orsanjiv@users.noreply.github.com>
2023-09-25 03:18:58 +0530

    upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <>
+      <div className="bg-slate-600">
+        <h1 className=" text-3xl bg-slate-600 italic flex flex-row-reverse">React-Tailwind-Template</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
